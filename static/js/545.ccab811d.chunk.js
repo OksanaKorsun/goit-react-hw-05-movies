c4100097eefd0824b1b7247cec347c0a7e1cf9d5
OksanaKorsun <128100267+OksanaKorsun@users.noreply.github.com>
2023-12-16 16:19:01 +0000

    Deploying to gh-pages from @ OksanaKorsun/goit-react-hw-05-movies@c87e7d01c5e5e76f83b16000fbb3eb2858ce09f7 🚀

--- static/js/545.ccab811d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[545],{545:function(t,e,a){a.d(e,{a_L:function(){return n}});var r=a(983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 32 32"},child:[{tag:"path",attr:{d:"M 10.8125 9.28125 L 4.09375 16 L 10.8125 22.71875 L 12.21875 21.28125 L 7.9375 17 L 28 17 L 28 15 L 7.9375 15 L 12.21875 10.71875 Z"}}]})(t)}}}]);
-//# sourceMappingURL=545.ccab811d.chunk.js.map
